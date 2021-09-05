ed6fd3b72505c4fac9fe1e8de93effc819bfed7b
Morimx <82421851+Morimx@users.noreply.github.com>
2021-09-05 13:56:29 -0300

    Cardboards genericos

--- dasenet/src/App.js
+++ dasenet/src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 import Navbar from "../src/components/NavBar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ItemListContainer from "./components/Container/ItemListContainer";
+import Item from "./components/Items/Item";
 
 
 function App() {
   return (
     <>
         <Navbar />
-      
       <ItemListContainer mensaje='Hola Bienvenidos' />
+      <Item />
     </>
   );
 }
