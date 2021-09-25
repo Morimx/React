1bd9696a08a36c0c1e9c9985b472ba06d5f874e6
Morimx <82421851+Morimx@users.noreply.github.com>
2021-09-25 19:40:34 -0300

    Mejoras de carrito

---
+++ dasenet/src/components/Cart/Carrito.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import { CartContextUse } from "../../Context/CartContext";
+
+export default function Carrito() {
+  const { clear, removeItem, cart } = CartContextUse();
+
+  return (
+    <>
+      <h1 className=" text-center fonts">Tu Carrito</h1>
+      <div>
+        <div className="d-flex justify-content-center">
+          <button className="btn buyBtn " onClick={clear}>
+            Limpiar Carrito
+          </button>
+          <Link to="/">
+            <button className="btn buyBtn ">Volver</button>
+          </Link>
+        </div>
+        <div className="container-fluid">
+          <div className="row">
+            {cart.map((unidad) => (
+              <div
+                className="row d-flex justify-content-center detail_container py-2"
+                key={unidad.item.id}
+              >
+                <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                  <img
+                    src={unidad.item.foto}
+                    className="ml-4 mb-3 imgDetail"
+                    alt={unidad.item.titulo}
+                  ></img>
+                </div>
+                <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
+                  <div className="d-flex text_container">
+                    <div className="bar"></div>
+                    <div className=" text-left">
+                      <h5 className="product_title fonts">
+                        {unidad.item.nombre}
+                      </h5>
+                      <p className="product_desc text-center fonts">
+                        Cantidad: {unidad.quantity}
+                      </p>
+                      <p className=" card-text text-center fonts">
+                        {unidad.quantity * unidad.item.precio} Precio Final
+                      </p>
+
+                      <div className="buy d-flex containerBtn">
+                        <button
+                          className="btn buyBtn"
+                          onClick={() => removeItem(unidad.item.id)}
+                        >
+                          Quitar Item
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
