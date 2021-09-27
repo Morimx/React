117f663b210aa4bd25b5111e33667bf399aa69bf
Morimx <82421851+Morimx@users.noreply.github.com>
2021-09-27 10:58:46 -0300

    Precio total

--- dasenet/src/components/Cart/Carrito.jsx
+++ dasenet/src/components/Cart/Carrito.jsx
@@ -3,7 +3,7 @@ import { CartContextUse } from "../../Context/CartContext";
 
 export default function Carrito() {
   const { clear, removeItem, cart } = CartContextUse();
-  /*const totalCarrito = cart.map(
+  const totalCarrito = cart.map(
     (unidad) => unidad.quantity * unidad.item.precio
   );
 
@@ -11,11 +11,13 @@ export default function Carrito() {
   for (let i = 0; i < totalCarrito.length; i++) {
     sumaTotal += totalCarrito[i];
   }
-*/
+
   return (
     <>
       <h1 className=" text-center fonts">Tu Carrito</h1>
-
+      <h2 className="fonts text-center">
+        El total de tu carrito es: {sumaTotal}
+      </h2>
       <div>
         <div className="d-flex justify-content-center">
           <button className="btn buyBtn " onClick={clear}>
